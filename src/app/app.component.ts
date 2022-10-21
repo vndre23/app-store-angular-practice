38a74e5df24da12470029d4fa63b32d8a375dea1
Andre Cruz <ajcruzg95@gmail.com>
2022-10-20 19:47:41 -0500

    intermedio-ciclo de vida

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,54 +1,27 @@
 import { Component } from '@angular/core';
-import { Product } from './product.model';
+import { Product } from './models/product.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  name = 'Andre';
-  age = 18;
-  btnDisabled = true;
-  person = {
-    name: 'Andre',
-    age: 27,
-  };
-  names: string[] = ['Nico', 'Juli', 'Santi'];
-  newName = '';
+  imgParent = '';
+  showImage = true;
   products: Product[] = [
     {
       id: 1,
       name: 'producto 1',
-      price: 20,
+      image:
+        'https://i.pinimg.com/550x/72/4d/80/724d80d10df742487b28d934c48ed8d6.jpg',
+      price: 11,
     },
   ];
-  register = {
-    name: '',
-    email: '',
-    password: '',
-  };
-  toggleButton = () => {
-    this.btnDisabled = !this.btnDisabled;
-  };
-  ageIncrease() {
-    this.person.age++;
+
+  onLoaded(img: string) {
+    console.log('log padre', img);
   }
-  onScroll(event: Event) {
-    const element = event.target as HTMLElement;
-    console.log(element.scrollTop);
-  }
-  changeName(event: Event) {
-    const element = event.target as HTMLInputElement;
-    this.person.name = element.value;
-  }
-  addName() {
-    this.names.push(this.newName);
-    this.newName = '';
-  }
-  deleteName(index: number) {
-    this.names.splice(index, 1);
-  }
-  onRegister() {
-    console.log(this.register);
+  toggleImg() {
+    this.showImage = !this.showImage;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImgComponent } from './components/img/img.component';
+import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ImgComponent, ProductComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
